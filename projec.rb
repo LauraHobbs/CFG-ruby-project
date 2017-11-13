9586e7cb38c722e90cd76b91115614f266b3ee12
Laura <lauraemily.hobbs@gmail.com>
2017-11-13 20:24:03 +0000

    Added in location
    So I added tweets in London- so we can fine tweets by an area!

--- projec.rb
+++ projec.rb
@@ -1,14 +1,19 @@
 require 'sinatra'
 require 'twitter'
 twitter_api = Twitter::REST::Client.new do |config|
-  config.consumer_key =
-  config.consumer_secret =
-  config.access_token =
-  config.access_token_secret = 
+  config.consumer_key = ""
+  config.consumer_secret = ""
+  config.access_token = "-"
+  config.access_token_secret = ""
 end
 
+search_options = {
+  result_type: "recent",
+  geocode: "51.50282,-0.11397,1mi"
+}
+
 get '/' do
-  @tweets = twitter_api.search("by:AlexHonnald yosemite").take(3)
+  @tweets = twitter_api.search("firework", search_options).take(5)
 
   erb :tweet_list
 end
